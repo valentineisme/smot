9d8bf4fecff7602f23504b6627b9765bbb981049
yurihs <yurisalvador@hotmail.com>
2019-12-06 15:18:31 -0300

    Favor não armazenar senhas em plain text :|

--- fapesc/models.py
+++ fapesc/models.py
@@ -49,33 +49,12 @@ class usuario(models.Model):
     # cidade = models.CharField(max_length=128)
     # estado = models.CharField(max_length=128)
     email = models.CharField(max_length=128)
-    senha = models.CharField(max_length=128, null=True)
     # formacao = models.CharField(max_length=128)
 
     class Meta:
         verbose_name = 'Usuario'
         verbose_name_plural = 'Usuarios'
 
-    def save(self):
-        if not self.id:
-            c = usuario.objects.filter(email=self.email).count()
-            if c:
-                raise Exception("Email Existente")
-
-            usr = User.objects.filter(username=self.email)
-            if usr:
-                u = usr[0]
-            else:
-                u = User.objects.create_user(self.email, self.email, self.senha)
-            u.save()
-            self.user = u
-        else:
-            self.user.username = self.email
-            self.user.email = self.email
-            self.user.set_password(self.senha)
-            self.user.save()
-
-        super(usuario, self).save()
 
     def __str__(self):
         return self.nome
